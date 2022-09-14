e0e65e6597dc8e6537ca2f76840b343fdb54c1a2
Jose Ricardo Romo <jricardoromo21@gmail.com>
2022-09-13 20:01:33 -0500

    fisrt commit

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+import imp
+from rest_framework import routers
+from .api import ProjectViewSet
+
+router = routers.DefaultRouter()
+
+router.register('api/project', ProjectViewSet, 'project')
+
+urlpatterns = router.urls
